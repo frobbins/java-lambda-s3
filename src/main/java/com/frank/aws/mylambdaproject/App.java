fce7f14ed0284d090e3efc2f23ee6be2da9070bc
Frank Robbins <george.robbins@coxautoinc.com>
2020-04-04 23:07:12 -0400

    simplified

--- src/main/java/com/frank/aws/mylambdaproject/App.java
+++ src/main/java/com/frank/aws/mylambdaproject/App.java
@@ -1,36 +1,67 @@
 package com.frank.aws.mylambdaproject;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.event.S3EventNotification;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 
-public class App extends BaseApp implements RequestHandler<S3Event, OutputBean> {
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
-    @Override
-    public OutputBean doWork() {
-        OutputBean outputBean;
-        getLambdaLogger().log(getStringFromS3Bucket(getS3FileName()));
+public class App implements RequestHandler<S3Event, String> {
+
+    private AmazonS3 amazonS3;
+    private String s3BucketName;
+    private LambdaLogger logger;
+    private String s3FileName;
+
+    public String handleRequest(S3Event event, Context context) {
+        String response = null;
+        initialize(event, context);
         try {
-            int fileCount = getListOfFilesFromS3().size();
-            outputBean = buildSuccessOutputBean(fileCount);
+            response = getStringFromS3Bucket();
         } catch ( Exception ex ) {
-            outputBean = buildFailureOutputBean(ex);
+            logStackTrace(ex);
+            log("Error happened: " + ex.getMessage());
         }
-        return outputBean;
+        return response;
     }
 
-    protected OutputBean buildFailureOutputBean(Exception ex) {
-        OutputBean outputBean = new OutputBean();
-        logStackTrace(ex);
-        outputBean.setStatus(ExecutionStatus.FAILURE);
-        return outputBean;
+    private void initialize(S3Event event, Context context) {
+        logger = context.getLogger();
+        amazonS3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
+        S3EventNotification.S3EventNotificationRecord record=event.getRecords().get(0);
+        s3BucketName = record.getS3().getBucket().getName();
+        s3FileName = record.getS3().getObject().getKey();
     }
 
-    protected OutputBean buildSuccessOutputBean(int fileCount) {
-        OutputBean outputBean = new OutputBean();
-        outputBean.setStatus(ExecutionStatus.SUCCESS);
-        outputBean.setBucketName(getS3BucketName());
-        outputBean.setFileCount(fileCount);
-        return outputBean;
+    protected String getStringFromS3Bucket() {
+        log("Fetching file contents!");
+        String contents = "None found.";
+        log("Lambda triggered with bucket: " + s3BucketName + " and filename: " + s3FileName);
+        if (amazonS3.doesObjectExist(s3BucketName, s3FileName)) {
+            log(" S3 file " + s3FileName + " found.");
+            contents = amazonS3.getObjectAsString(s3BucketName, s3FileName);
+        } else {
+            throw new AmazonS3Exception(" Failed to get string from S3 bucket: " + s3BucketName + " File Key: " + s3FileName);
+        }
+        return contents;
+    }
+
+    protected void log(String value) {
+        logger.log(value);
+    }
+
+    protected void logStackTrace(Exception ex) {
+        StringWriter swriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(swriter);
+        ex.printStackTrace(writer);
+        log(swriter.toString());
     }
 
 
